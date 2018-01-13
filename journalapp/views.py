6a76f18f65cbb42fd08f3d5371f1375e85135d73
BolajiOlajide <bolaji.olajide@andela.com>
2018-01-13 21:42:53 +0100

    implement vue controller

--- journalapp/views.py
+++ journalapp/views.py
@@ -4,7 +4,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        'days': [1, 2, 3],
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
